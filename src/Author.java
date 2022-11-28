8155db2188bec17bed06e3453b4f5196e10f1e56
Dustin Zook <dustin.w.zook@gmail.com>
2022-11-28 14:54:33 -0600

    completed exercise

--- src/Author.java
+++ src/Author.java
@@ -5,16 +5,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Author extends Person {
-    private List books;
+    private final List<String> books;
 
     public Author(String firstName, String lastName) {
         super(firstName, lastName);
-        books = new ArrayList();
+        books = new ArrayList<>();
     }
 
     /**
      * @deprecated Use publishedBooks instead
      */
+
+    @SuppressWarnings("deprecation")
     @Deprecated
     public List<String> getBooks() {
         return books;
@@ -28,7 +30,7 @@ public class Author extends Person {
         books.add(book);
     }
 
-    @Override
+
     public String sortName() {
         return String.format("%s, %s", lastName, firstName);
     }
